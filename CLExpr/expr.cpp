f8fc1479ba86c622b172ea9bad548b909425d80b
Victor Efimov <tp7ptr@gmail.com>
2014-09-21 18:05:51 +1100

    Fix context creation being broken in some cases

--- CLExpr/expr.cpp
+++ CLExpr/expr.cpp
@@ -252,18 +252,20 @@ ClExpr::ClExpr(PClip clip1, PClip clip2, PClip clip3,
     }
 
     //init ocl
-    cl_platform_id platform[3];
+    cl_platform_id platforms[3];
     cl_uint platforms_count;
-    cl_int error = clGetPlatformIDs(3, platform, &platforms_count);
+    cl_int error = clGetPlatformIDs(3, platforms, &platforms_count);
     CHECK_CL_ERROR(error, env);
 
     cl_device_id device;
     cl_int last_error;
+    cl_platform_id platform;
     for (cl_uint i = 0; i < platforms_count; ++i)
     {
-        last_error = clGetDeviceIDs(platform[i], CL_DEVICE_TYPE_GPU, 1, &device, NULL);
+        last_error = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_GPU, 1, &device, NULL);
         if (last_error == CL_SUCCESS)
         {
+            platform = platforms[i];
             break;
         }
     }
